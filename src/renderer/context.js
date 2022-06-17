b72999a10fecf2076ae00c2259ff6d86d7b07877
jzy <1271639879@qq.com>
2022-06-17 09:42:25 +0800

    feat: 创建renderer

---
+++ src/renderer/context.js
@@ -0,0 +1,19 @@
+import {
+  createSVGElement,
+  mount
+} from "./util";
+
+export function createContext(width, height) {
+  const svg = createSVGElement('svg');
+  svg.setAttribute("width", width);
+  svg.setAttribute("height", height);
+  svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
+
+  const g = createSVGElement('g');
+  mount(svg, g);
+
+  return {
+    node: svg,
+    group: g
+  }
+}
