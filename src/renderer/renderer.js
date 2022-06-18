163f56435ba81e78187dcc8a7e2618392eca1612
jzy <1271639879@qq.com>
2022-06-18 15:48:12 +0800

    feat: 完善renderer

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -2,6 +2,9 @@ import { createContext } from './context';
 import {
   line, circle, text, rect, path, ring,
 } from './shape';
+import {
+  restore, save, scale, translate, rotate,
+} from './transform';
 
 export function createRenderer(width, height) {
   const context = createContext(width, height);
@@ -12,5 +15,12 @@ export function createRenderer(width, height) {
     rect: (options) => rect(context, options),
     path: (options) => path(context, options),
     ring: (options) => ring(context, options),
+    restore: () => restore(context),
+    save: () => save(context),
+    scale: (...args) => scale(context, ...args),
+    rotate: (...args) => rotate(context, ...args),
+    translate: (...args) => translate(context, ...args),
+    node: () => context.node,
+    group: () => context.group,
   };
 }
